871c799cd7e5fe0d1118e6f21b3b298d60746435
NickGconroc <NickGconroc@users.noreply.github.com>
2025-11-24 10:17:37 +0100

    seguir en casa

---
+++ src/main/java/daw/Paqueteria.java
@@ -0,0 +1,19 @@
+package daw;
+
+public class Paqueteria {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static double calcultarTarifaEnvio(double peso, String zona){
+        
+            while (peso<=0) {
+                System.out.println("Dame un numero valido");
+            }
+            
+
+        return 0;
+
+    }
+}

a003fbe43224093aa29823ca69698a9b53b38987
Rodrigo <235055043+NickGconroc@users.noreply.github.com>
2025-12-01 22:36:23 +0100

    listo para implementar codigo

--- src/main/java/daw/Paqueteria.java
+++ src/main/java/daw/Paqueteria.java
@@ -1,19 +1,6 @@
 package daw;
 
 public class Paqueteria {
-    public static void main(String[] args) {
 
-
-    }
-
-    public static double calcultarTarifaEnvio(double peso, String zona){
-        
-            while (peso<=0) {
-                System.out.println("Dame un numero valido");
-            }
-            
-
-        return 0;
-
-    }
 }
+ç
--- src/main/java/daw/Paqueteriatest.java
+++
@@ -1,5 +0,0 @@
-package daw;
-
-public class Paqueteriatest {
-
-}
